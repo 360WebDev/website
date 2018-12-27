be3b74749aa819f81c2fb933d4310473b74be5ab
Arnaud POINTET <arnaud@oipnet.info>
2018-12-27 16:09:23 +0100

    feat(Use spatie media library to manage avatars)

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,6 +21,7 @@
                     @auth
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="{{ auth()->user()->getMedia('avatars')->first()->getUrl('thumb') }}">
                                 {{ auth()->user()->name }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
